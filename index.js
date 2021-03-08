f25b6d622d55817ac948b17d0e81da9f202eb07d
Nathan Cullen <45045903+NathanJCullen@users.noreply.github.com>
2021-03-08 09:09:58 +0000

    begin resposnive design

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 $("h1").click(function (e) {
-    $(this).css({'color' : 'red'})
-  });
+  $(this).css({ color: "red" });
+});
+
+$(".selection").click(function (e) {
+  $(this).css({ background: "black" });
+  $(this).siblings().css({ background: "white" });
+  let target = "." + $(this).attr("id");
+  $(target).css({ display: "block" });
+  $(target).siblings().css({ display: "none" });
+});
